d63cd0fd5beb205047b034a1eb5b651472e2ba44
anasmhmd <116856719+anasmhmd@users.noreply.github.com>
2024-02-14 11:20:50 +0530

    Add files via upload

---
+++ sum/myfile.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class myfile
+{
+public static void main(String args[])
+ {
+   int a,b;
+   Scanner input = new Scanner(System.in);
+   a=input.nextInt();
+   b=input.nextInt();
+   System.out.println("sum="+(a+b));
+   }
+ }  
